8161b1bc5cc0e3cde37f963436929dd0ed1e9e86
a3510377 <a102009102009@gmail.com>
2023-11-17 22:18:27 +0800

    Revert "Update generate.py"
    This reverts commit c5d45e906e7af62c91c1300dfbff73c37882bafa.

--- scripts/generate.py
+++ scripts/generate.py
@@ -15,9 +15,6 @@ OUT_DIR.mkdir()
 
 infos: list[str] = []
 for dir_path in BASE_DIR.iterdir():
-    class_out_dir = tmp = OUT_DIR / dir_path.name
-    class_out_dir.mkdir(parents=True, exist_ok=True)
-
     for path in dir_path.iterdir():
         info = ""
 
@@ -32,15 +29,15 @@ for dir_path in BASE_DIR.iterdir():
             for asset_file in assets_dir.iterdir():
                 filename = asset_file.relative_to(assets_dir)
                 tmp = f"assets/{basename}{filename}"
-                info = info.replace(f"./assets/{filename}", tmp)
                 out_path = OUT_DIR / tmp
+                info = info.replace(f"./assets/{filename}", tmp)
                 out_path.parent.mkdir(parents=True, exist_ok=True)
                 shutil.copy(asset_file, out_path)
 
         for file in [*path.glob("*.bsf"), *path.glob("*.v")]:
             if (OUT_DIR / file.name).is_file():
                 print(f"[WARNING] Skipping file {file.name!r}, file is already")
-            shutil.copy(file, class_out_dir / file.name)
+            shutil.copy(file, OUT_DIR)
 
         infos.append(info)
 
